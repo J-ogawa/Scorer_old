04de40603d4345a33f81374815f5e1379df6c31e
J-ogawa <conilus@gmail.com>
2016-02-23 01:30:54 +0900

    Add project

---
+++ Scorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scorer
+//
+//  Created by conilus on 2/23/16.
+//  Copyright © 2016 debussynetwork. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
